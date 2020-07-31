436f90907184df26f9bcd79126669ab7466a887c
AlexanderPeraltaL <68553117+AlexanderPeraltaL@users.noreply.github.com>
2020-07-31 08:42:48 -0500

    m

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 document.getElementById("btn-guardar-inmueble").addEventListener('click', (e) =>{
 	if(document.getElementById("frm-mensualidad").reportValidity()){
-        let nombre, documento, genero, fecha_nac;
+        var nombre, documento, genero, fecha_nac, nombre_almacenado, documento_almacenado,
+            genero_almacenado, fecha_nac_almacenada;
+        nombre = document.getElementById("nombre-propietario");
+        localStorage.setItem("nombre-propietario", nombre.value);
+        nombre_almacenado = localStorage.getItem("nombre-propietario");
+
+        documento = document.getElementById("doc-propietario");
+        localStorage.setItem("doc-propietario", documento.value);
+        documento_almacenado = localStorage.getItem("doc-propietario");
+
+        genero = document.getElementById("genero-propietario");
+        localStorage.setItem("genero-propietario", nombre.value);
+        genero_almacenado = localStorage.setItem("genero-propietario", nom;
+
+
+        fecha_nac = document.getElementById("fecha-nac-propietario");
+        localStorage.setItem("nombre-propietario", nombre.value);
+
+
+        console.log(nombre_almacenado);        
     }else{
         //alert("Error de validación de campos.")
     }
@@ -15,4 +34,3 @@ $(document).ready((e) => {
         $('#modal-adicionar').modal('toggle');
     });
 });
-
